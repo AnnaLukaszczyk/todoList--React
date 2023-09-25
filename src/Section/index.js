e1477bcc7c933f3d56057e9bba7a3c10af6631dd
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-09-25 13:05:17 +0200

    chanded names of classes

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,11 +2,11 @@ import "./style.css"
 
 const Section = ({title, body, extraHeaderContent}) => (
     <section className="section">
-				<div className="section__area">
+				<div className="section__header">
 					<h2 className="section__title">{title}</h2>
 					{extraHeaderContent}
 				</div>
-				<div className="tasksArea">
+				<div className="section__body">
 					{body}
 				</div>
 			</section>
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -8,7 +8,7 @@ const Tasks = ({tasks, hideDoneTasks}) => (
                <button className="task__button">
 					<i className={`fa-solid fa-check${task.done ? "" : " task__buttonIcon"}`}></i>
 				</button>
-				<span className={`task__content${task.done ? " taskDone" : ""}`}>{task.content}</span>
+				<span className={`task__content${task.done ? " task__content--done" : ""}`}>{task.content}</span>
 				<button className="task__button task__button--remove">
 					<i className="fa-regular fa-trash-can"></i>
 				</button> 
