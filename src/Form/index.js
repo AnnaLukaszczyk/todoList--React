c02f70d500345e059a6a8cea403aebac73e22ca6
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-10-26 19:12:39 +0200

    import styles from styled.js

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./style.css";
+import { StyledForm, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
 	const [newTaskContent, setNewTaskContent] = useState("");
@@ -15,15 +15,14 @@ const Form = ({ addNewTask }) => {
 		setNewTaskContent("");
 	};
 	return (
-		<form className="form" onSubmit={onFormSubmit}>
-			<input
+		<StyledForm onSubmit={onFormSubmit}>
+			<Input
 				value={newTaskContent}
-				className="form__input"
 				placeholder="Co jest do zrobienia?"
 				onChange={({ target }) => setNewTaskContent(target.value)}
 			/>
-			<button className="form__button">Dodaj zadanie</button>
-		</form>
+			<Button>Dodaj zadanie</Button>
+		</StyledForm>
 	);
 };
 
