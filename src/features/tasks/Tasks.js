40c4c23357d5cc711617468d58ac5a135151eb7c
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-11-30 18:00:01 +0100

    useSelector added

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import Form from "./Form/index.js";
 import TaskList from "./TaskList/index.js";
 import Buttons from "./Buttons/index.js";
@@ -5,17 +6,18 @@ import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
 import { useTasks } from "../../useTasks.js";
+import { selectTasks } from "./tasksSlice";
 
 function Tasks() {
 	const {
-		tasks,
-		hideDone,
-		toggleHideDone,
+		// tasks,
 		removeTask,
 		toggleTaskDone,
 		setAllDone,
 		addNewTask,
 	} = useTasks();
+
+	const { tasks } = useSelector(selectTasks);
 
 	return (
 		<Container>
@@ -30,17 +32,12 @@ function Tasks() {
 				title="Lista zadań"
 				body={
 					<TaskList
-						tasks={tasks}
-						hideDone={hideDone}
 						removeTask={removeTask}
 						toggleTaskDone={toggleTaskDone}
 					/>
 				}
 				extraHeaderContent={
 					<Buttons
-						tasks={tasks}
-						hideDone={hideDone}
-						toggleHideDone={toggleHideDone}
 						setAllDone={setAllDone}
 					/>
 				}
