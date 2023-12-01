799260e989ab56ce3ac7b7c77ac6d08c8f868323
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-12-01 14:35:00 +0100

    code formatted

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -6,26 +6,14 @@ import Header from "../../common/Header";
 import Container from "../../common/Container";
 
 function Tasks() {
-
-
 	return (
 		<Container>
 			<Header title="Lista zadań" />
-
-			<Section
-				title="Dodaj nowe zadanie"
-				body={<Form />}
-			/>
-
+			<Section title="Dodaj nowe zadanie" body={<Form />} />
 			<Section
 				title="Lista zadań"
-				body={
-					<TaskList
-					/>
-				}
-				extraHeaderContent={
-					<Buttons />
-				}
+				body={<TaskList />}
+				extraHeaderContent={<Buttons />}
 			/>
 		</Container>
 	);
