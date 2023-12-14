d449ccb7d89d62a740b5f577f5da77c0579bf37e
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-12-14 18:21:09 +0100

    Author.js added

---
+++ src/features/author/Author.js
@@ -0,0 +1,14 @@
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+const Author = () => (
+  <Container>
+			<Header title="O autorze" />
+			<Section 
+      title="Anna Łukaszczyk" 
+      body={<><p>tu uzupełnić</p></>} />
+		</Container>
+)
+
+export default Author;
