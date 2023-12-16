0b63064a57867e1e676cedd98ab678ae8c438cc8
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-12-16 18:16:18 +0100

    added task subpage and task details display handling

---
+++ src/features/tasks/TaskPage/index.js
@@ -0,0 +1,20 @@
+import Section from "../../../common/Section/index.js";
+import Header from "../../../common/Header/index.js";
+import Container from "../../../common/Container/index.js";
+import { useParams } from "react-router-dom/cjs/react-router-dom.js";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice.js";
+
+function TaskPage() {
+	const { id } = useParams();
+	const task = useSelector((state) => getTaskById(state, id));
+
+	return (
+		<Container>
+			<Header title="Szczegóły zadania" />
+			<Section title={task ? task.content : "Nie znaleziono zadania 😢"} body={<><strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}</>} />
+		</Container>
+	);
+}
+
+export default TaskPage;
