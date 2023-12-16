2dd0c5d4e2dcf9be3f493fc23cb26a10d5514afd
Anna Lukaszczyk <normalnie4@tlen.pl>
2023-12-16 18:21:12 +0100

    task subpage added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom";
-import Tasks from "./features/tasks/Tasks";
-import Author from "./features/author/Author";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
 
 const App = () => (
 	<HashRouter>
@@ -14,15 +15,18 @@ const App = () => (
 				</li>
 			</ul>
 			<Switch>
+			<Route path="/zadania/:id">
+					<TaskPage />
+				</Route>
 				<Route path="/zadania">
-					<Tasks />
+					<TasksPage />
 				</Route>
 				<Route path="/autor">
-					<Author />
+					<AuthorPage />
 				</Route>
-        <Route path="/">
-                <Redirect to="/zadania" />
-            </Route>
+				<Route path="/">
+					<Redirect to="/zadania" />
+				</Route>
 			</Switch>
 		</nav>
 	</HashRouter>
